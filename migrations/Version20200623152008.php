f181c1ba118c6de4659e20c966f3430b7d31d2ea
Florian Moser <git@famoser.ch>
2020-06-23 17:21:25 +0200

    generate openapi spec

--- migrations/Version20200623152008.php
+++ migrations/Version20200623152008.php
@@ -10,11 +10,11 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200621083419 extends AbstractMigration
+final class Version20200623152008 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'create event, question, setting, task and user table';
+        return '';
     }
 
     public function up(Schema $schema): void
@@ -22,6 +22,8 @@ final class Version20200621083419 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE event (id CHAR(36) NOT NULL --(DC2Type:guid)
         , name CLOB NOT NULL, start_at DATETIME NOT NULL, created_at DATETIME NOT NULL, last_changed_at DATETIME NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE product (id CHAR(36) NOT NULL --(DC2Type:guid)
+        , name CLOB NOT NULL, category INTEGER NOT NULL, active BOOLEAN NOT NULL, created_at DATETIME NOT NULL, last_changed_at DATETIME NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE TABLE question (id CHAR(36) NOT NULL --(DC2Type:guid)
         , text CLOB NOT NULL, repetition INTEGER NOT NULL, created_at DATETIME NOT NULL, last_changed_at DATETIME NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE TABLE setting (id CHAR(36) NOT NULL --(DC2Type:guid)
@@ -30,22 +32,16 @@ final class Version20200621083419 extends AbstractMigration
         , name CLOB NOT NULL, interval_in_days INTEGER NOT NULL, reward INTEGER NOT NULL, last_execution_at DATETIME DEFAULT NULL, created_at DATETIME NOT NULL, last_changed_at DATETIME NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE TABLE user (id CHAR(36) NOT NULL --(DC2Type:guid)
         , name CLOB NOT NULL, pin INTEGER NOT NULL, karma INTEGER NOT NULL, created_at DATETIME NOT NULL, last_changed_at DATETIME NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('ALTER TABLE product ADD COLUMN active BOOLEAN NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE event');
+        $this->addSql('DROP TABLE product');
         $this->addSql('DROP TABLE question');
         $this->addSql('DROP TABLE setting');
         $this->addSql('DROP TABLE task');
         $this->addSql('DROP TABLE user');
-        $this->addSql('CREATE TEMPORARY TABLE __temp__product AS SELECT id, name, category, created_at, last_changed_at FROM product');
-        $this->addSql('DROP TABLE product');
-        $this->addSql('CREATE TABLE product (id CHAR(36) NOT NULL --(DC2Type:guid)
-        , name CLOB NOT NULL, category INTEGER NOT NULL, created_at DATETIME NOT NULL, last_changed_at DATETIME NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('INSERT INTO product (id, name, category, created_at, last_changed_at) SELECT id, name, category, created_at, last_changed_at FROM __temp__product');
-        $this->addSql('DROP TABLE __temp__product');
     }
 }
